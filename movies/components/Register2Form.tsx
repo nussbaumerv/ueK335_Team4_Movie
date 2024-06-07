b21842f459b26a0dfea74a1ec75bec6b39e56c25
Valentin Nussbaumer <mail@valentin-nussbaumer.com>
2024-06-07 15:18:07 +0200

    Adds register pages and updates navigater and login

---
+++ movies/components/Register2Form.tsx
@@ -0,0 +1,164 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { Formik, FormikProps } from 'formik';
+import { RegisterAPIRequest } from '../service/Auth';
+import { useNavigation, useRoute, Link } from '@react-navigation/native';
+
+interface FormValues {
+  surname: string;
+  lastname: string;
+  age: number;
+}
+
+const Register2Form: React.FC = () => {
+  const route = useRoute();
+  console.log(route.params);
+  const { email, password } = route.params as { email: string; password: string };
+  const navigation = useNavigation();
+  const [registerError, setRegisterError] = useState<string | null>(null);
+
+  const validate = (values: FormValues) => {
+    const errors: { surname?: string; lastname?: string; age?: string } = {};
+
+    if (!values.surname) {
+      errors.surname = 'Surname is required';
+    }
+
+    if (!values.lastname) {
+      errors.lastname = 'Lastname is required';
+    }
+
+    if (!values.age) {
+      errors.age = 'Age is required';
+    } else if (!/^\d+$/.test(values.age)) {
+      errors.age = 'Age must be a number';
+    }
+
+    return errors;
+  };
+
+  return (
+    <Formik
+      initialValues={{ surname: '', lastname: '', age: 0 }}
+      validate={validate}
+      onSubmit={(values, { setSubmitting }) => {
+        setRegisterError(null);
+        RegisterAPIRequest()
+          .getAuthToken(email, password, values.surname, values.lastname, values.age)
+          .then(() => {
+            console.log('Successful');
+            navigation.navigate('Navbar');
+          })
+          .catch((error) => {
+            if (error.response && error.response.status === 400) {
+              setRegisterError('Email is already used.');
+            } else {
+              setRegisterError('An unexpected error occurred. Please try again.');
+              console.error('An error occurred:', error);
+            }
+          })
+          .finally(() => {
+            setSubmitting(false);
+          });
+      }}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        isSubmitting,
+      }: FormikProps<FormValues>) => (
+        <View style={styles.container}>
+          <Text style={styles.title}>Register</Text>
+          <TextInput
+            style={styles.input}
+            label="Age"
+            mode="outlined"
+            onChangeText={handleChange('age')}
+            onBlur={handleBlur('age')}
+            value={values.age}
+            keyboardType="numeric"
+            error={touched.age && !!errors.age}
+          />
+          {touched.age && errors.age && (
+            <Text style={styles.errorText}>{errors.age}</Text>
+          )}
+          
+          <TextInput
+            style={styles.input}
+            label="Surname"
+            mode="outlined"
+            onChangeText={handleChange('surname')}
+            onBlur={handleBlur('surname')}
+            value={values.surname}
+            error={touched.surname && !!errors.surname}
+          />
+          {touched.surname && errors.surname && (
+            <Text style={styles.errorText}>{errors.surname}</Text>
+          )}
+
+          <TextInput
+            style={styles.input}
+            label="Lastname"
+            mode="outlined"
+            onChangeText={handleChange('lastname')}
+            onBlur={handleBlur('lastname')}
+            value={values.lastname}
+            error={touched.lastname && !!errors.lastname}
+          />
+          {touched.lastname && errors.lastname && (
+            <Text style={styles.errorText}>{errors.lastname}</Text>
+          )}
+
+
+          {registerError && <Text style={styles.errorText}>{registerError}</Text>}
+
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={handleSubmit}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Registering...' : 'Register'}
+          </Button>
+          <Text>Already have an Account? <Link style={styles.link} to={{screen: 'LoginForm'}}>Login here</Link></Text>
+        </View>
+      )}
+    </Formik>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    fontFamily: 'Roboto',
+    margin: 20,
+  },
+  input: {
+    width: 320,
+    marginBottom: 12,
+  },
+  button: {
+    width: 148,
+    margin: 20,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 12,
+  },
+  link: {
+    color: '#D0BCFF',
+  }
+});
+
+export default Register2Form;
--- movies/navigation/MainNavigator.tsx
+++ movies/navigation/MainNavigator.tsx
@@ -1,34 +1,41 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 import HomePage from '../components/pages/HomePage';
 import MoviesPage from '../components/pages/MoviesPage';
 import ProfilePage from '../components/pages/ProfilePage';
+import Register1Form from '../components/Register1Form';
+import Register2Form from '../components/Register2Form';
+import LoginForm from '../components/LoginForm';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 // User has to be logged in, if not directed to login page
 
- function Tabs() {
-    return(
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Movies" component={MoviesPage} />
-        <Tab.Screen name="Profile" component={ProfilePage} />
-    </Tab.Navigator>
+function Tabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomePage} />
+            <Tab.Screen name="Movies" component={MoviesPage} />
+            <Tab.Screen name="Profile" component={ProfilePage} />
+        </Tab.Navigator>
     );
-    } 
+}
 
-    function MainStackNavigator() {
-    return(
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="HomePage" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Navbar" component={Tabs} />
-        </Stack.Navigator>
-    </NavigationContainer>
+function MainStackNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Register" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Register1Form" component={Register1Form} />
+                <Stack.Screen name="Register2Form" component={Register2Form} />
+                <Stack.Screen name="LoginForm" component={LoginForm} />
+                <Stack.Screen name="Navbar" component={Tabs} />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
-    } 
- 
-    export default MainStackNavigator; 
+}
+
+export default MainStackNavigator; 
