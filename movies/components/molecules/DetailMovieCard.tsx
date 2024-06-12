4c993d53275603a89e4588aba0a55bbc8006ad12
Dominic Heule <dominic.heule@noseryoung.com>
2024-06-12 11:40:17 +0200

    hidding the header

--- movies/components/molecules/DetailMovieCard.tsx
+++ movies/components/molecules/DetailMovieCard.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react';
-import { useState } from 'react';
 import { View, Dimensions, StyleSheet, ScrollView } from 'react-native';
 import { Card, IconButton, Text, useTheme } from 'react-native-paper';
 
 const MovieDetailCard = ({ movie, onDelete, onEdit, onBack }) => {
     const theme = useTheme();
-    const [isScrolledToEnd, setIsScrolledToEnd] = useState(false);
 
     const styles = StyleSheet.create({
         container: {
             flex: 1,
-            position: 'relative',
-            backgroundColor: 'black',
+            position: 'absolute',
+            backgroundColor: '',
             justifyContent: 'center',
             alignItems: 'center',
+            width: '100%', // Ensure the container covers the entire screen width
+            height: '100%', // Ensure the container covers the entire screen height
         },
         card: {
-            marginTop: Dimensions.get('window').height * 0.15,
+            marginTop: Dimensions.get('window').height * 0.05,
             width: Dimensions.get('window').width * 0.8,
             backgroundColor: '#141218', // Card background color
             borderRadius: 12,
             overflow: 'hidden',
+        },
+        contentContainer: {
+            overflow: 'hidden', 
+            borderRadius: 12,
         },
         header: {
             flexDirection: 'row',
@@ -33,6 +37,13 @@ const MovieDetailCard = ({ movie, onDelete, onEdit, onBack }) => {
             position: 'absolute',
             left: 10,
             top: Dimensions.get('window').height * 0.03,
+            width: 40,
+            height: 40,
+            backgroundColor: '#393540',
+            borderRadius: 12,
+            borderWidth: 0,
+            shadowColor: 'transparent',
+            elevation: 0,
         },
         title: {
             paddingTop: 30,
@@ -97,61 +108,46 @@ const MovieDetailCard = ({ movie, onDelete, onEdit, onBack }) => {
             shadowColor: 'transparent',
             elevation: 0,
         },
-        scrollIndicator: {
-            fontSize: 24,
-            textAlign: 'center',
-            color: 'white',
-            marginVertical: 10,
-        },
     });
-
-    const handleScroll = (event) => {
-        const { layoutMeasurement, contentOffset, contentSize } = event.nativeEvent;
-        const isScrolledToEnd = layoutMeasurement.height + contentOffset.y >= contentSize.height - 10;
-        setIsScrolledToEnd(isScrolledToEnd);
-    };
 
     return (
         <View style={styles.container}>
             <Card style={styles.card}>
-                <View style={styles.header}>
-                    <IconButton
-                        icon="arrow-left"
-                        mode="contained"
-                        onPress={onBack}
-                        style={styles.backButton}
-                        iconColor="white"
-                    />
-                    <Text style={styles.title}>{movie.title}</Text>
+                <View style={styles.contentContainer}>
+                    <View style={styles.header}>
+                        <IconButton
+                            icon="arrow-left"
+                            mode="contained"
+                            onPress={onBack}
+                            style={styles.backButton}
+                            iconColor="white"
+                        />
+                        <Text style={styles.title}>{movie.title}</Text>
+                    </View>
+                    <View style={styles.imageContainer}>
+                        <Card.Cover source={{ uri: movie.thumbnail }} style={styles.image} />
+                    </View>
+                    <ScrollView style={styles.scrollViewContent}>
+                        <Card.Content>
+                            <Text style={styles.subtitle}>Extract</Text>
+                            <Text style={styles.normalText}>{formatExtract(movie.extract)}</Text>
+                            <Text style={styles.subtitle}>Cast</Text>
+                            {movie.cast.map((cast, index) => (
+                                <Text key={index} style={styles.castText}>
+                                    {cast}
+                                </Text>
+                            ))}
+                            <Text style={styles.subtitle}>Year</Text>
+                            <Text style={styles.normalText}>{movie.year}</Text>
+                            <Text style={styles.subtitle}>Genres</Text>
+                            <Text style={styles.normalText}>{movie.genres.join(', ')}</Text>
+                        </Card.Content>
+                    </ScrollView>
+                    <Card.Actions style={styles.actions}>
+                        <IconButton icon="delete" size={24} onPress={onDelete} style={styles.deleteButton} iconColor="white" />
+                        <IconButton icon="pen" size={24} onPress={onEdit} style={styles.editButton} iconColor="white" />
+                    </Card.Actions>
                 </View>
-                <View style={styles.imageContainer}>
-                    <Card.Cover source={{ uri: movie.thumbnail }} style={styles.image} />
-                </View>
-                <ScrollView
-                    style={styles.scrollViewContent}
-                    onScroll={handleScroll}
-                    scrollEventThrottle={16}
-                >
-                    <Card.Content>
-                        <Text style={styles.subtitle}>Extract</Text>
-                        <Text style={styles.normalText}>{formatExtract(movie.extract)}</Text>
-                        <Text style={styles.subtitle}>Cast</Text>
-                        {movie.cast.map((cast, index) => (
-                            <Text key={index} style={styles.castText}>
-                                {cast}
-                            </Text>
-                        ))}
-                        <Text style={styles.subtitle}>Year</Text>
-                        <Text style={styles.normalText}>{movie.year}</Text>
-                        <Text style={styles.subtitle}>Genres</Text>
-                        <Text style={styles.normalText}>{movie.genres.join(', ')}</Text>
-                    </Card.Content>
-                    {!isScrolledToEnd && <Text style={styles.scrollIndicator}>...</Text>}
-                </ScrollView>
-                <Card.Actions style={styles.actions}>
-                    <IconButton icon="delete" size={24} onPress={onDelete} style={styles.deleteButton} iconColor="white" />
-                    <IconButton icon="pen" size={24} onPress={onEdit} style={styles.editButton} iconColor="white" />
-                </Card.Actions>
             </Card>
         </View>
     );
--- movies/navigation/MoviesStackNavigator.tsx
+++ movies/navigation/MoviesStackNavigator.tsx
@@ -7,7 +7,7 @@ const Stack = createStackNavigator();
 
 export default function MoviesStackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="MoviesList" component={MoviesPage} />
       <Stack.Screen name="MovieDetail" component={MovieDetail} />
     </Stack.Navigator>
