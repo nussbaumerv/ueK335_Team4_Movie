4edfe0ebde4507488371f069be70db7f440f0e6f
Dominic Heule <dominic.heule@noseryoung.com>
2024-06-12 17:07:40 +0200

    adding the filter functionality to the moviespage

--- movies/components/pages/MoviesPage.tsx
+++ movies/components/pages/MoviesPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Button } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Modal } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { useTheme, Divider, IconButton } from 'react-native-paper'; 
+import { useTheme, Divider, IconButton, Button } from 'react-native-paper'; 
 import CustomButton from '../atoms/CustomButton';
 import AddButton from '../atoms/AddButton';
 import LogoutButton from '../atoms/LogoutButton';
@@ -9,19 +9,24 @@ import MovieCard from '../molecules/MovieCard';
 import { useEffect, useState } from 'react';
 import { MovieAPI } from '../../service/Movie';
 import { MovieType } from '../../types/Movie';
-import MovieDetail from '../MovieDetail';
 
 export default function MoviePage() {
   const navigation = useNavigation();
   const theme = useTheme();
-  const scrollViewRef = React.useRef();
-  const [showBackToTop, setShowBackToTop] = React.useState(false);
-  
-  const handleScroll = (event : any) => {
+  const scrollViewRef = React.useRef<ScrollView>(null);
+  const [showBackToTop, setShowBackToTop] = React.useState<boolean>(false);
+  const [movies, setMovies] = useState<MovieType[] | null>(null);
+  const [filteredMovies, setFilteredMovies] = useState<MovieType[] | null>(null);
+  const [showFilterModal, setShowFilterModal] = useState<boolean>(false);
+  const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
+  const [expandGenre, setExpandGenre] = useState(false);
+  const [expandYear, setExpandYear] = useState(false);
+  const [expandedModalHeight, setExpandedModalHeight] = useState(0);
+
+  const handleScroll = (event: any) => {
     const { y } = event.nativeEvent.contentOffset;
     setShowBackToTop(y > 0); 
   };
-  const [movies, setMovies] = useState<MovieType[] | null>(null);
 
   useEffect(() => {
     const loadMovies = async () => {
@@ -29,7 +34,7 @@ export default function MoviePage() {
         const movieApi = MovieAPI();
         const fetchedMovies = await movieApi.getMovies();
         setMovies(fetchedMovies);
-        console.log(movies)
+        setFilteredMovies(fetchedMovies);
       } catch (error) {
         console.error('Error fetching movies', error);
       }
@@ -38,12 +43,61 @@ export default function MoviePage() {
     loadMovies();
   }, []);
 
+  const filterMoviesByYear = (year : string) => {
+    if (year) {
+      const filtered = movies?.filter(movie => movie.year === Number(year));
+      setFilteredMovies(filtered);
+      closeFilterModal();
+    } else {
+      setFilteredMovies(movies);
+    }
+  };
+
+  const filterMoviesByGenre = (selectedGenre : string) => {
+    if (selectedGenre) {
+      const filtered = movies?.filter(movie => movie.genres?.includes(selectedGenre));
+      setFilteredMovies(filtered);
+      closeFilterModal();
+    } else {
+      setFilteredMovies(movies); // If no genre is selected, show all movies
+    }
+  };
+
+  const toggleGenreFilter = () => {
+    setExpandGenre(!expandGenre);
+    if (!expandGenre) {
+      setExpandYear(false);
+    }
+  };
+
+  const toggleYearFilter = () => {
+    setExpandYear(!expandYear);
+    if (!expandYear) {
+      setExpandGenre(false);
+    }
+  };
+
+  const years = [
+      "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012",
+      "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000",
+  ];
+
   const handleMoviePress = (movie: MovieType) => {
     navigation.navigate('MovieDetail', { id: movie.id });
   };
 
+  const openFilterModal = () => {
+    setShowFilterModal(true);
+  };
+
+  const closeFilterModal = () => {
+    setShowFilterModal(false);
+    setExpandedModalHeight(0);
+  };
+
   const styles = StyleSheet.create({
     container: {
+
       flex: 1,
       backgroundColor: theme.colors.onBackground,
       position: 'relative',
@@ -74,7 +128,7 @@ export default function MoviePage() {
       backgroundColor: '#49454F', 
     },
     movieContainer: {
-      marginTop: Dimensions.get('window').height * 0.225,
+      marginTop: Dimensions.get('window').height * 0.225 + expandedModalHeight + 20,
     },
     backToTopButton: {
       position: 'absolute',
@@ -96,12 +150,88 @@ export default function MoviePage() {
       fontSize: 24,
       color: 'white',
     },
-    
+    filterText: {
+      fontSize: 20,
+      marginBottom: 5,
+    },
+    filterButton: {
+
+    },
+    filterScrollView: {
+      maxHeight: 300, 
+      marginTop: 10, 
+    },
+    backButton : {
+      position: 'absolute',
+      top: 5,
+      right: 10,
+      zIndex: 1,
+    },
+    filterHeader: {
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+    },
+    expandButton: {
+      marginLeft: 10,
+    },
+    modalContainer: {
+      flex: 1,
+      backgroundColor: theme.colors.onBackground, 
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    filterModal: {
+      backgroundColor: '#141218', 
+      padding: 20,
+      position: 'relative',
+      top: 200,
+      borderRadius: 12,
+      justifyContent: 'center',
+    },
+    buttonContent: {
+      color: 'white',
+    },
+    filterOption: {
+      fontSize: 18,
+      marginBottom: 5,
+      color: 'white', 
+    },
+    modalButtons : {
+      color: 'white',
+      backgroundColor: '#141218',
+      width: '80%',
+      borderRadius: 12,
+      textAlign: 'left'
+    }
   });
 
   const handleBackToTop = () => {
-    scrollViewRef.current.scrollTo({ y: 0, animated: true });
+    scrollViewRef.current?.scrollTo({ y: 0, animated: true });
   };
+
+  const genres = [
+    "Action",
+    "Comedy",
+    "Drama",
+    "Horror",
+    "Supernatural",
+    "Science Fiction",
+    "Crime",
+    "Mystery",
+    "Thriller",
+    "Adventure",
+    "Fantasy",
+    "War",
+    "Superhero",
+    "Family",
+    "Romance",
+    "Historical",
+    "Political",
+    "Teen",
+    "Animated",
+    "Sports"
+  ];
 
   return (
     <View style={styles.container}>
@@ -111,7 +241,7 @@ export default function MoviePage() {
           <AddButton onPress={() => navigation.navigate('Home', { name: 'Home' })} iconName="plus" />
         </View>
         <View style={styles.buttonWrapper}>
-          <CustomButton onPress={() => navigation.navigate('Screen2', { name: 'Screen2' })} iconName="filter" />
+          <CustomButton onPress={openFilterModal} iconName="filter" />
         </View>
         <View style={[styles.buttonWrapper, { marginRight: 0 }]}>
           <CustomButton onPress={() => navigation.navigate('Screen3', { name: 'Screen3' })} iconName="sort" />
@@ -130,8 +260,8 @@ export default function MoviePage() {
         indicatorStyle="white" 
         onScroll={handleScroll}
       >
-        {movies &&
-          movies.map((movie) => (
+        {filteredMovies &&
+          filteredMovies.map((movie) => (
             <TouchableOpacity key={movie.id} onPress={() => handleMoviePress(movie)}>
               <MovieCard movie={movie} />
             </TouchableOpacity>
@@ -142,6 +272,47 @@ export default function MoviePage() {
           <IconButton icon="arrow-up" iconColor={styles.arrowUpIcon.color}/>
         </TouchableOpacity>
       )}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showFilterModal}
+        onRequestClose={closeFilterModal}
+      >
+        <View>
+        <View style={styles.filterModal} onLayout={(event) => {
+  const { height } = event.nativeEvent.layout;
+  setExpandedModalHeight(height);
+}}>
+            <IconButton
+              icon="close"
+              iconColor="red"
+              size={24}
+              onPress={closeFilterModal}
+              style={styles.backButton}
+            />
+            <View style={styles.filterHeader}>
+              <Button textColor="white" onPress={toggleGenreFilter} icon={expandGenre ? "arrow-down" : "arrow-right"} style={styles.modalButtons} >
+                Genres
+              </Button>
+              <Button textColor="white" onPress={toggleYearFilter} icon={expandYear ? "arrow-down" : "arrow-right"} style={styles.modalButtons}>
+                Years
+              </Button>
+            </View>
+            <ScrollView style={styles.filterScrollView}>
+              {expandGenre && genres.map((genre, index) => (
+                <TouchableOpacity key={index} onPress={() => filterMoviesByGenre(genre)}>
+                  <Text style={styles.filterOption}>{genre}</Text>
+                </TouchableOpacity>
+              ))}
+              {expandYear && years.map((year, index) => (
+                <TouchableOpacity key={index} onPress={() => filterMoviesByYear(year)}>
+                  <Text style={styles.filterOption}>{year}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
